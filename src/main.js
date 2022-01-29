f7eb811bea8f4effaf7d1d883e176b8b10556310
ADAUTO LUCAS <adautosl.lucas@gmail.com>
2022-01-29 19:49:41 -0300

    adicionado bootstrap vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import PrimeVue from 'primevue/config'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 
 
 
-createApp(App).use(router, PrimeVue).mount('#app')
+createApp(App).use(router, BootstrapVue, IconsPlugin).mount('#app')
 
